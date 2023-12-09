022fe0309a1c93872a1762662c82727004ff9046
Duy.Dao <lukhach1990@gmail.com>
2023-12-09 22:06:56 +0700

    merge Khang php ui

--- resources/views/create_job.blade.php
+++ resources/views/create_job.blade.php
@@ -1,80 +1,102 @@
 @extends('layouts.app')
 @section('content')
-    <link rel="stylesheet" href="{{ asset('assets/css/create-job.css') }}">
-    <main id="main">
-        <div data-aos="fade" class="page-title">
-            <div class="heading ps-4 ">
-                <h1 class="text-white ">Đăng tin tuyển dụng</h1>
-            </div>
-        </div>
-        <section>
-            <div class="container">
-                <form action="{{ route('job.store') }}" method="post">
-                    @csrf
-                    <div class="form-floating mb-4">
-                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"
+<link rel="stylesheet" href="{{ asset('assets/css/create-job.css') }}">
+<main id="main">
+  <div data-aos="fade" class="page-title">
+    <div class="heading ps-4 ">
+      <h1 class="text-white ">Đăng tin tuyển dụng</h1>
+    </div>
+  </div>
+  <section>
+      <div class="container">
+          <form action="{{ route('job.store') }}" method="post">
+@csrf
+          <div class="form-floating mb-4">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"
                             name="title"></textarea>
-                        <label for="floatingTextarea2">Tiêu đề công việc</label>
-                        @error('title')
+            <label for="floatingTextarea2">Tiêu đề công việc</label>
+@error('title')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
-                    </div>
-                    <div class="form-floating mb-4">
-                        <h4>Điểm mạnh của công ty</h4>
+          </div>
+          <div class="form-floating mb-4">
+            <h4>Điểm mạnh của công ty</h4>
                         <textarea class="editor" name="strength" id="strength" contenteditable="true"></textarea>
                         @error('strength')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
-                    </div>
-                    <div class="form-floating mb-4">
-                        <h4>Mô tả công việc</h4>
+          </div>
+          <div class="form-floating mb-4">
+            <h4>Mô tả công việc</h4>
                         <textarea class="editor" id="description" name="description" contenteditable="true"></textarea>
                         @error('description')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
-                    </div>
-                    <div class="form-floating mb-4">
-                        <h4>Yêu cầu công việc</h4>
+          </div>
+          <div class="form-floating mb-4">
+            <h4>Yêu cầu công việc</h4>
                         <textarea class="editor" id="required" name="required" contenteditable="true"></textarea>
                         @error('required')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
-                    </div>
-                    <div class="form-floating mb-4">
-                        <h4>Tại sao bạn lại làm việc tại đây</h4>
+          </div>
+          <div class="form-floating mb-4">
+            <h4>Tại sao bạn lại làm việc tại đây</h4>
                         <textarea class="editor" id="reason" name="reason" contenteditable="true"></textarea>
                         @error('reason')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
-                    </div>
-                    <div class="mb-3">
-                        <input type="number" class="form-control" id="luong" name="luong" placeholder="Nhập lương">
-                    </div>
-                    <div class="mb-3">
-                      <input type="number" class="form-control" id="minSalary" name="salryMin" placeholder="Lương tối thiểu">
-                  </div>
-                  <div class="mb-3">
-                    <input type="number" class="form-control" id="maxSalary" name="salaryMax" placeholder="Lương tối đa">
-                </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="activation" value="active" id="flexRadioDefault1" checked>
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            Active
-                        </label>
-                    </div>
-                    <div class="form-check mb-4">
-                        <input class="form-check-input" type="radio" name="activation" value="unactive" id="flexRadioDefault2">
-                        <label class="form-check-label" for="flexRadioDefault2">
-                            Unactive
-                        </label>
-                    </div>
-                    <div class="mb-3">
-                        <input type="submit" class="btn btn-primary" value="Đăng tin">
-                    </div>
-                </form>
-            </div>
-        </section>
-        <script>
+          </div>
+          <div class="mb-3">
+            <input type="number" class="form-control" id="luong" name="luong" placeholder="Nhập lương">
+          </div>
+          <div class="mb-3">
+            <input type="number" class="form-control" id="minSalary" name="salryMin" placeholder="Lương tối thiểu">
+        </div>
+        <div class="mb-3">
+          <input type="number" class="form-control" id="maxSalary" name="salaryMax" placeholder="Lương tối đa">
+        </div>
+          <div class="mb-3">
+              <p class="fw-bold">Hình thức làm việc</p>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="WorkType" value="Remote" id="remote">
+                <label class="form-check-label" for="remote">
+                      Remote
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="WorkType" value="Offline" id="offline" checked>
+                <label class="form-check-label" for="offline">
+                  Offline
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="WorkType" value="Remote & Offline" id="both" >
+                <label class="form-check-label" for="both">
+                  Both
+                </label>
+              </div>
+          </div>
+          <div class="mb-3">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="activation" value="active" id="flexRadioDefault1" checked>
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Active
+                </label>
+              </div>
+              <div class="form-check mb-4">
+                <input class="form-check-input" type="radio" name="activation" value="unactive" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                  Unactive
+                </label>
+                        </div>
+          <div class="mb-3">
+            <input type="submit" class="btn btn-primary" value="Đăng tin">
+          </div>
+        </form>
+      </div>
+  </section>
+<script>
     var elements = document.getElementsByClassName('editor');
     for (var i = 0; i < elements.length; i++) {
     CKEDITOR.replace( elements[i],{
@@ -98,7 +120,7 @@
     });
   }
 </script>
-    </main>
+</main>
 @endsection
 
     
--- resources/views/layouts/header_login.blade.php
+++ resources/views/layouts/header_login.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang2="en">
-<head>
+  <head>
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
@@ -16,8 +16,8 @@
     <link href="https://fonts.googleapis.com" rel="preconnect" />
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin />
     <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet" />
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+      rel="stylesheet" />
 
     <!-- Vendor CSS Files -->
     <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
@@ -28,10 +28,10 @@
 
     <!-- Template Main CSS File -->
     <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet"/>
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
     <script>
@@ -51,86 +51,89 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
-</head>
+  </head>
 
 <body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu">
 <header id="header" class="header fixed-top d-flex align-items-center ">
-      <div
+    <div
         class="container-fluid d-flex align-items-center justify-content-between"
       >
         <a
           href="index.php"
-          class="logo d-flex align-items-center me-auto me-xl-0"
+           class="logo d-flex align-items-center me-auto me-xl-0"
         >
-          <!-- Uncomment the line below if you also wish to use an image logo -->
-          <!-- <img src="assets/img/logo.png" alt=""> -->
-          <h1>Find Your Job</h1>
-          <span>.</span>
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1>Find Your Job</h1>
+            <span>.</span>
         </a>
 
         <!-- Nav Menu -->
         <nav id="navmenu" class="navmenu">
-          <ul>
-            <li><a href="{{route('home')}}" class="active">Trang chủ</a></li>
-            <li><a href="{{ route('search')}}">Tìm việc làm</a></li>
-            <li class="dropdown has-dropdown">
-              <a href="#"
+            <ul>
+                <li><a href="{{route('home')}}" class="active">Trang chủ</a></li>
+                <li><a href="{{ route('search')}}">Tìm việc làm</a></li>
+                <li class="dropdown has-dropdown">
+                    <a href="#"
                 ><span>Dropdown</span> <i class="bi bi-chevron-down"></i
               ></a>
-              <ul class="dd-box-shadow">
-                <li><a href="#"></a></li>
-                <li class="dropdown has-dropdown">
-                  <a href="#"
+                    <ul class="dd-box-shadow">
+                        <li><a href="#"></a></li>
+                        <li class="dropdown has-dropdown">
+                            <a href="#"
                     ><span>Theo kĩ năng</span>
-                    <i class="bi bi-chevron-down"></i
+                                <i class="bi bi-chevron-down"></i
                   ></a>
-                  <ul class="dd-box-shadow">
-                    <li><a href="#">Theo kĩ năng</a></li>
-                    <li><a href="#">Java </a></li>
-                    <li><a href="#">.NET </a></li>
-                    <li><a href="#">Python</a></li>
-                    <li><a href="#">React</a></li>
-                  </ul>
+                            <ul class="dd-box-shadow">
+                                <li><a href="#">Theo kĩ năng</a></li>
+                                <li><a href="#">Java </a></li>
+                                <li><a href="#">.NET </a></li>
+                                <li><a href="#">Python</a></li>
+                                <li><a href="#">React</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Theo kinh nghiệm</a></li>
+                        <li><a href="#">Dropdown 3</a></li>
+                        <li><a href="#">Dropdown 4</a></li>
+                    </ul>
                 </li>
-                <li><a href="#">Theo kinh nghiệm</a></li>
-                <li><a href="#">Dropdown 3</a></li>
-                <li><a href="#">Dropdown 4</a></li>
-              </ul>
-            </li>
-            <li><a href="index.php#contact">Contact</a></li>
-          </ul>
+                <li><a href="index.php#contact">Contact</a></li>
+            </ul>
 
-          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
         <!-- End Nav Menu -->
         <div class="header-button-group d-flex  flex-row gap-3 ">
-          <div class="user-info">
-            <nav id="navmenu" class="navmenu">
-                <ul>
-                    <li class="dropdown has-dropdown">
-                        <div class="d-flex flex-row justify-align-content-around align-items-center gap-2 ">
-                            <img src="{{ asset('assets/img/blog/blog-author-2.jpg')}}" alt="" class="">
-                            <p class="text-light fw-bold m-0 gap-2 ">{{ auth()->user()->username}}</p>
-                        </div>
-                        <ul class="dd-box-shadow">
-                            <li><a href="{{ route('profile')}}">Hồ sơ của tôi</a></li>
-                            <li><a href="#">Công việc của tôi</a></li>
-                            <li><a href="{{ route('logout') }}">Đăng xuất</a></li>
-                        </ul>
-                    </li>
-                </ul>
+            <div class="user-info">
+                <nav id="navmenu" class="navmenu">
+                    <ul>
+                        <li class="dropdown has-dropdown">
+                            <div class="d-flex flex-row justify-align-content-around align-items-center gap-2 ">
+                                <img src="{{ asset('assets/img/blog/blog-author-2.jpg')}}" alt="" class="">
+                                <p class="text-light fw-bold m-0 gap-2 ">{{ auth()->user()->username}}</p>
+                            </div>
 
-                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-            </nav>
-          </div>
-          <div class="user-info d-flex flex-row justify-content-center gap-2 align-content-center text-light fw-bold  ">
-            <a href="#"><p>VI</p></a>
-            <span> / </span>
-            <a href="#"><p>EN</p></a>
-          </div>
+                            
+                            <ul class="dd-box-shadow">
+                                <li><a href="#">Hồ sơ của tôi</a></li>
+                                <li><a href="#">Công việc của tôi</a></li>
+                                <li><a href="create-job.php">Đăng tin tuyển dụng</a></li>
+                                <li><a href="#">Đăng xuất</a></li>
+                            </ul>
+                          </li>
+                    </ul>
+
+                    <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                </nav>
+            </div>
+            <div class="user-info d-flex flex-row justify-content-center gap-2 align-content-center text-light fw-bold  ">
+                <a href="#"><p>VI</p></a>
+                <span> / </span>
+                <a href="#"><p>EN</p></a>
+            </div>
         </div>
-      </div>
-  </header>
+    </div>
+</header>
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered ">
       <div class="modal-content">
