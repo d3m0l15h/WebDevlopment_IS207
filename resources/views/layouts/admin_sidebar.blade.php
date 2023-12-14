5c506fc17b42158be51fd2c92a522bae5529c315
d3m0l15h <davicmax12@gmail.com>
2023-12-15 01:09:54 +0700

    routing changed

--- resources/views/layouts/admin_sidebar.blade.php
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -2,19 +2,19 @@
     <h4 class="text-center fw-bold ">DASHBOARD</h4>
     <ul class="list-group list-group-flush mt-4 ">
        <li class="list-group-item">
-          <a href="admin-employ-manage.php" class="tab d-flex flex-row align-items-center ">
+          <a href="{{route('admin.user')}}" class="tab d-flex flex-row align-items-center ">
              <img src="{{ asset('assets/img/user.png')}}" alt="" width="30" height="30" class="me-4">
              <p>User</p>
           </a>
        </li>
        <li class="list-group-item d">
-          <a href="admin-company-manage.php" class="tab d-flex flex-row align-items-center">
+          <a href="{{route('admin.employer')}}" class="tab d-flex flex-row align-items-center">
              <img src="{{ asset('assets/img/portfolio.png')}}" alt="" width="30" height="30" class="me-4">
              <p>Employer</p>
           </a>
        </li>
        <li class="list-group-item d">
-          <a href="admin-request.php" class="tab d-flex flex-row align-items-center">
+          <a href="{{route('admin.request')}}" class="tab d-flex flex-row align-items-center">
              <img src="{{ asset('assets/img/portfolio.png')}}" alt="" width="30" height="30" class="me-4">
              <p>Yêu cầu đăng kí NTD</p>
           </a>
