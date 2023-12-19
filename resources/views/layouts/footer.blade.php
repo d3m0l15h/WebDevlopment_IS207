a2ba8f4c86db4eab12c3a0bf1556786901e5b3e1
Khang <khangdev2813@gmail.com>
2023-12-19 17:22:00 +0700

    update footer + homepage

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,13 +2,13 @@
     <div class="container footer-top">
         <div class="row gy-4">
             <div class="col-lg-5 col-md-12 footer-about">
-                <a href="/" class="logo d-flex align-items-center">
-                    <span>Append</span>
+                <a href="{{route('home')}}" class="logo d-flex align-items-center me-auto me-xl-0">
+                    <h1>Finding Job</h1>
+                    <span class="special-dot">.</span>
                 </a>
+    
                 <p>
-                    Cras fermentum odio eu feugiat lide par naso tierra. Justo eget
-                    nada terra videa magna derita valies darta donna mare fermentum
-                    iaculis eu non diam phasellus.
+                    Kết nối việc làm, kiến tạo tương lai
                 </p>
                 <div class="social-links d-flex mt-4">
                     <a href=""><i class="bi bi-twitter"></i></a>
@@ -21,41 +21,28 @@
             <div class="col-lg-2 col-6 footer-links">
                 <h4>Useful Links</h4>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About us</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Terms of service</a></li>
-                    <li><a href="#">Privacy policy</a></li>
+                    <li><a href="{{ route('home') }}">Trang chủ</a></li>
+                    <li><a href="{{ route('jobs') }}">Jobs</a></li>
+                    <li><a href="{{ route('home') }}#contact">Liên hệ</a></li>
                 </ul>
             </div>
-
-            <div class="col-lg-2 col-6 footer-links">
-                <h4>Our Services</h4>
-                <ul>
-                    <li><a href="#">Web Design</a></li>
-                    <li><a href="#">Web Development</a></li>
-                    <li><a href="#">Product Management</a></li>
-                    <li><a href="#">Marketing</a></li>
-                    <li><a href="#">Graphic Design</a></li>
-                </ul>
-            </div>
-
             <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-                <h4>Contact Us</h4>
-                <p>A108 Adam Street</p>
-                <p>New York, NY 535022</p>
-                <p>United States</p>
-                <p class="mt-4">
-                    <strong>Phone:</strong> <span>+1 5589 55488 55</span>
+                <h4>Liên hệ với chung tôi</h4>
+                <p>93/3 Hoàng Diệu</p>
+                <p>Phường Linh Trung, Thủ Đức, Thành phố Hồ Chí Minh
                 </p>
-                <p><strong>Email:</strong> <span>info@example.com</span></p>
+                <p>Việt Nam</p>
+                <p class="mt-4">
+                    <strong>Phone:</strong> <span>+84 0865 904 101</span>
+                </p>
+                <p><strong>Email:</strong> <span>findjob@gmail.com</span></p>
             </div>
         </div>
     </div>
 
     <div class="container copyright text-center mt-4">
         <p>
-            &copy; <span>Copyright</span> <strong class="px-1">Append</strong>
+            &copy; <span>Copyright</span> <strong class="px-1">Finding Job.</strong>
             <span>All Rights Reserved</span>
         </p>
         <div class="credits">
@@ -63,7 +50,7 @@
             <!-- You can delete the links only if you've purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            Designed by <a href="https://bootstrapmade.com/">G10</a>
         </div>
     </div>
 </footer>
